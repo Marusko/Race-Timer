ae78bf1b1d0d54c27e2af63d88339286c21cf2fb
Matúš Suský <matussusky@gmail.com>
2023-04-07 15:37:42 +0200

    Version 1.6.0 -> 1.6.1

--- RR_Timer/UI/MainWindow.xaml.cs
+++ RR_Timer/UI/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace RR_Timer.UI
     public partial class MainWindow
     {
         private const string Author = "Matúš Suský";
-        private const string Version = "1.6.0";
+        private const string Version = "1.6.1";
         private readonly ClockLogic _clockLogic;
         private readonly ScreenHandler _screenHandler;
         private Window? _clockWindow;
