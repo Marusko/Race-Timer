bc9aa967f1880ec935c1382ec3728d015ed6f184
Matúš Suský <matussusky@gmail.com>
2023-05-11 20:34:42 +0200

    Version 1.8.4 -> 1.8.6

--- RR_Timer/UI/MainWindow.xaml.cs
+++ RR_Timer/UI/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace Race_timer.UI
     public partial class MainWindow
     {
         private const string Author = "Matúš Suský";
-        private const string Version = "1.8.4";
+        private const string Version = "1.8.6";
         private readonly ClockLogic _clockLogic;
         private readonly ScreenHandler _screenHandler;
         private Window? _clockWindow;
