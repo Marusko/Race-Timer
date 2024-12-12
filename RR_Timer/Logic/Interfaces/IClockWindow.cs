2b395f11c37294697296f784396c406fbafcc378
Marusko <matussusky@gmail.com>
2024-12-12 18:18:35 +0100

    Added clock window interface

---
+++ RR_Timer/Logic/Interfaces/IClockWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Race_timer.Logic.Interfaces
+{
+    public interface IClockWindow
+    {
+        void TimerTickLogic();
+        void TimerClickLogic();
+        void OnTimerClick();
+        void SetLabels(string name, string type);
+        void SetImage(BitmapImage image);
+        void SetCodeImage(BitmapSource image);
+        void SetChildren(UserControl alignment);
+    }
+}
